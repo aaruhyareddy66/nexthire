edf5dd548363b10e25e4c9c72d43086e6cc6522f
aaruhyareddy66 <aaruhyareddy66@gmail.com>
2026-06-26 12:46:07 +0530

    fixed cors for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://nexthire-sooty.vercel.app",
+        "https://nexthire-git-main-self27.vercel.app",
+        "https://nexthire-f22gtn7gk-self27.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
